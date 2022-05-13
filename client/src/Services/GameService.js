60d8d61eeed8f95fff5c9318dd63c9d901cd4223
Parker Hudson <gphudson97@hotmail.com>
2022-05-13 16:40:23 -0400

    Added middleware for Game object endpoints

--- client/src/Services/GameService.js
+++ client/src/Services/GameService.js
@@ -1,5 +1,27 @@
 export default {
-	getGames: () => {},
+	getGames: () => {
+		return fetch("/api/getGames").then(() => {
+			if (response.status != 401) {
+				return response.json().then((data) => data);
+			} else {
+				return { message: { msgBody: "UnAuthorized" }, msgError: true };
+			}
+		});
+	},
 
-	addGame: (game) => {},
+	addGame: (game) => {
+		return fetch("/api/addGame", {
+			method: "post",
+			body: JSON.stringify(game),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then(() => {
+			if (response.status != 401) {
+				return response.json().then((data) => data);
+			} else {
+				return { message: { msgBody: "Unauthorized" }, msgError: true };
+			}
+		});
+	},
 };
